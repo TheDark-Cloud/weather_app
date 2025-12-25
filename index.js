ca2cfbd35c4a03dcbc35e392988646207c17a234
TheDark-Cloud <117770466+TheDark-Cloud@users.noreply.github.com>
2025-12-25 23:55:01 +0000

    weather app

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ const defaultCity = "paris";
 
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric"
 
+function weatherInfo(data){
+    ocument.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.humidity').innerHTML = data.main.humidity;
+    document.querySelector('.wind').innerHTML = `${data.wind.speed} km/h`;
+    document.querySelector('.temp').innerHTML = `${Math.round(data.main.temp)} °C`;
+}
+
+
 
 async function getWeather() {
-    console.log("clicked");
     const cityName = document.getElementById("cityName").value.trim();
 
     const city = cityName || defaultCity;
 
     const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
     const data = await response.json();
-    document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.humidity').innerHTML = data.main.humidity;
-    document.querySelector('.wind').innerHTML = `${data.wind.speed} km/h`;
-    document.querySelector('.temp').innerHTML = `${Math.round(data.main.temp)} °C`;
-    console.log(data);
 
-    switch (data.weather.main) {
+    weatherInfo(data);
+
+    switch (data.weather[0].main) {
         case "Rain":
             document.getElementById('weatherCondition').src = "images/rain.png";
             break;
@@ -30,7 +34,12 @@ async function getWeather() {
             break;
         case "Snow":
             documrnt.getElementById('weatherCondition').src = "images/snow.png";
-
+            break;
+        case "Drizzle":
+            document.getElementById('weatherCondition').src = "images/drizzle.png";
+            break;
+        case "Mist":
+            document.getElementById('weatherCondition').src = "images/mist.png";
     }
 }
 
